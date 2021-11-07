e139cf08ed83b4de359e1c3be3a2b93f2d08d8a5
abhioo7 <guptaabhi9650@gmail.com>
2021-11-07 11:24:23 +0530

    Updates

---
+++ precache-manifest.7f94dc4ea95e794d27c10d2ef91c2260.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56372f50523e916aa7e809d440641972",
+    "url": "/MSTRAssignment/index.html"
+  },
+  {
+    "revision": "488b60fdc751995b9e3c",
+    "url": "/MSTRAssignment/static/css/main.86817a72.chunk.css"
+  },
+  {
+    "revision": "ad6619a32403f5fbaecc",
+    "url": "/MSTRAssignment/static/js/2.8fd50471.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/MSTRAssignment/static/js/2.8fd50471.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "488b60fdc751995b9e3c",
+    "url": "/MSTRAssignment/static/js/main.b321a3da.chunk.js"
+  },
+  {
+    "revision": "9d9d9c54d08abc7346e1",
+    "url": "/MSTRAssignment/static/js/runtime-main.7dbbc6c2.js"
+  }
+]);
